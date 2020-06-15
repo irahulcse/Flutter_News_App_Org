a2a36987a534ba57b4209ec86e8152b3d58ba7a2
Rahul Chandra <1rahulchandra1@gmail.com>
2020-06-15 10:15:06 +0530

    Automatic Commit 2020-06-15-10:15:01

---
+++ android/app/src/main/kotlin/com/example/flutter_news_api_org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_news_api_org
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/helper/news.dart
@@ -0,0 +1,67 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_news_api_org/models/article.dart';
+import 'dart:convert';
+
+import 'package:flutter_news_api_org/secret.dart';
+
+class News {
+  List<Article> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/everything?q=bitcoin&from=2020-05-15&sortBy=publishedAt&apiKey=4b679205e4dd4550b087e2aa17063dd3";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
+
+class NewsForCategorie {
+  List<Article> news = [];
+
+  Future<void> getNewsForCategory(String category) async {
+    /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey}";*/
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=4b679205e4dd4550b087e2aa17063dd3";
+        
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          Article article = Article(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            urlToImage: element['urlToImage'],
+            publshedAt: DateTime.parse(element['publishedAt']),
+            content: element["content"],
+            articleUrl: element["url"],
+          );
+          news.add(article);
+        }
+      });
+    }
+  }
+}
